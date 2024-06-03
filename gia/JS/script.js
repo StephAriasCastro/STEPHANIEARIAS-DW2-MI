9dc1f761c4e79e6bb4bb187f331175bb882ad1c8
Ale <168048154+StephAriasCastro@users.noreply.github.com>
2024-06-03 15:57:05 -0300

    Desafio #2
    Stephanie Alejandra Arias Castro

---
+++ gia/JS/script.js
@@ -0,0 +1,10 @@
+let nombreProdcuto
+let precioUnitario
+let cantidadDeseada
+
+nombreProdcuto="plantas"
+precioUnitario=200
+
+cantidadDeseada=prompt("Ingrese la cantidad");
+
+alert("Su total es de $"+precioUnitario*cantidadDeseada+" por "+cantidadDeseada+" "+nombreProdcuto);
